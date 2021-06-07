35255592e32a8cbdd2d9d959dc67a642b8376668
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-06-07 18:56:28 +0200

    added ejs as view engine

--- server/app.js
+++ server/app.js
@@ -2,5 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+//SET
+app.set("view engine", "ejs");
+
+app.get("/", async (req, res) => {
+    res.render("pages/index", {Hello: "TJO"});
+  });
+
 app.listen(port, () => console.log(`Example app listening on port port!`))
